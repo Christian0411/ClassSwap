df01832c9dc18c69689b8f9018dcf72a02adbe9e
omart075 <omart075@fiu.edu>
2017-01-30 18:00:26 -0500

    Modified internalApi

--- api/internalApi.js
+++ api/internalApi.js
@@ -20,12 +20,59 @@ var getStudentInfo = function()
       }
       else
       {
-        console.log(res);
+        //student counter
+        var sCount = 0;
+        //class counter
+        var cCount = 0;
+        var hasClass = "";
+
+        //iterates through each student
+        //stops once all students have been iterated through
+        while(res[sCount] != null)
+        {
+            //gets the array of Has classes for each student
+            console.log(res[sCount].csp.Has);
+
+            //iterates through each class a student has
+            //stops once all classes have been iterated through
+            while(res[sCount].csp.Has[cCount] != null)
+            {
+              console.log(res[sCount].csp.Has[cCount]);
+              //gets each individual class in Has array
+              hasClass = res[sCount].csp.Has[cCount];
+              getClassInfo(hasClass);
+              cCount++;
+            }
+
+            sCount++;
+            cCount = 0;
+        }
         resolve(res);
       }
     });
   });
 
+}
+
+//function to get class info; called from within getStudentInfo
+var getClassInfo = function (classId)
+{
+    query = N1qlQuery.fromString('SELECT * FROM csp WHERE meta(csp).id LIKE "' + classId + '"')
+    return new Promise((resolve,reject) =>
+    {
+      bucket.query(query, function(err,res)
+      {
+        if(err)
+        {
+          console.log(err);
+          reject("Error");
+        }
+        else
+        {
+          resolve(res);
+        }
+      });
+    });
 }
 
 var login = function(user, pass)
